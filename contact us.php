838266cf4a972d248612758231b96fecdc596972
saurabhjoshispj <62388274+saurabhjoshispj@users.noreply.github.com>
2020-06-11 10:51:15 +0530

    Add files via upload

---
+++ contact us.php
@@ -0,0 +1,43 @@
+<link rel="shortcut icon" href="icon.png" type="image/x-icon">
+    <link rel="icon" href="icon.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+    <style>
+body { 
+    background-image: url("3.jpg");
+    height: 100vh;
+    background-size: cover; 
+    background-position: center;
+ }
+</style>  <header>
+        <div id="mobile-menu-close"><link rel="stylesheet" type="text/css" href="styles.css">
+            <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+            <li>
+              <a href="index.php"><i class="fa fa-home" aria-hidden="true"></i></a>
+            </li>
+            
+            
+            
+        </ul>
+    </header>
+    <!-- End header -->
+ <div id="contact">
+        <h2>Send Your Queries Here </h2>
+        <div id="contact-form">
+            <form method="POST" action="https://formspree.io/saurabhjoshibd@gmail.com">
+                <input type="hidden" name="_subject" value="Contact request from personal website" />
+                <input type="email" name="_subject" placeholder="Your email" required>
+                 <input type="mobile" name="_subject" placeholder="Your mobile number" required>
+                <textarea name="message" placeholder="Your message" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+          <h2>  Contact us At :- <br><a href="tel:7558201023"><h2><i class="fa fa-phone" aria-hidden="true"></i>
+<b><u> 7558201023</h1></a>
+        </div>
+        <!-- End #contact-form -->
+    </div>
+    <!-- End #contact -->
---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hospital web page</title> 
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="icon"  href="icon.png" type="image/x-icon">
+</head>
+  <body>
+ <div id="mobile-menu-open" class="shadow-large">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- End #mobile-menu-toggle -->
+    <body onload="myFunction()">
+<div id="loading"></div>
+    <header>
+        <div id="mobile-menu-close">
+            <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+            <li>
+                <a href="index.php"><i class="fa fa-home" aria-hidden="true"></i>
+Home</a>
+            </li>
+            <li>
+                <a href="index7.php"><i class="fa fa-question" aria-hidden="true"></i>
+Help</a>
+            </li>
+         
+            <li>
+                <a href="about.php"><i class="fa fa-info-circle" aria-hidden="true"></i>
+About</a>
+            </li>
+            
+            <li>
+                <a href="contact us.php"><i class="fa fa-phone-square" aria-hidden="true"></i>
+Contact us.</a>
+            </li>
+        </ul>
+    </header>
+    <!-- End header -->
+	
+<style>
+body { 
+	background-image:linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)), url("1.jpg");
+	height: 100vh;
+ 	background-size: cover; 
+ 	background-position: center;
+ }
+</style>
+<style>
+.button {
+  background-color: green; /* cyan */
+  border: 2px solid black;
+  color: white;
+  padding: 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 25px;
+  margin: 20px 10px;
+  cursor: pointer;
+  position: center;
+}
+
+.button1 {width: 500px;}
+.button2 {width: 500px;}
+.button3 {width: 500px;}
+.button4 {width: 500px;}
+
+
+</style>
+
+<center><br><br><br><h1> <mark> -: ANALYSIS ON MEDICINES AND DOCTORS AVAILABILITY IN HOSPITAL :- </h1></center><hr>
+<br><br><center>
+
+  <a href="index2.php">
+  
+ <button class="button button1">Patients corner</button></a><br>
+ <a href="index4.php">
+<button class="button button2">Receiptionist corner</button></a><br>
+<a href="index7.php">
+<button class="button button3">Help</button></a><br>
+<a href="about.php">
+<button class="button button4">About</button></a><br>
+</center>
+
+</body>
+</html>
